90f81092a9ec9d635ba1f3459bc05f657fc6de14
Andrew0407 <andres.inventum@gmail.com>
2019-11-26 10:57:59 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mhhtrv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mhhtrv)
---
+++ src/app/products.ts
@@ -0,0 +1,25 @@
+export const products = [
+  {
+    nombre: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    nombre: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    nombre: 'Phone Standard',
+    price: 299,
+    description: 'Are you mad?'
+
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
